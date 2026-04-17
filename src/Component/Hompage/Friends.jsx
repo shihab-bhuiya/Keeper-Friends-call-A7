660e15e201ca636820ca3a1d1ca8f37957e59c60
shihab-bhuiya <shihab.bhuiya07@gmail.com>
2026-04-17 22:54:32 +0600

    card design optimize

--- src/Component/Hompage/Friends.jsx
+++ src/Component/Hompage/Friends.jsx
@@ -2,7 +2,7 @@ import React, { use } from 'react';
 import Friend from './Friend';
 
 
-const friendsPromis = fetch("/public/friendsData.json").then(res => res.json());
+const friendsPromis = fetch("/friendsData.json").then(res => res.json());
 
 const Friends = () => {
 
@@ -11,7 +11,7 @@ const Friends = () => {
     return (
         <div className='max-w-350 container mx-auto px-4 md:px-8 py-10 '>
             <h2 className='text-xl md:text-2xl font-semibold mb-6'>Your Friends</h2>
-            <div className='grid gap-5 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4'>
+            <div className='grid gap-5 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 shadow-olive-300 rounded-lg p-4 bg-white'>
                 {friends.map(friend => 
                     <Friend key={friend.id} friend={friend} ></Friend>
 
